3e0246774f6184331ae0b67d43e0ec74c196a455
Devesh Lamichhane <deveshl@protonmail.com>
2021-02-27 14:43:12 +0545

    added script.sh

--- script.sh
+++ script.sh
@@ -27,6 +27,7 @@ $command $packages
 key_status=`gpg --list-secret-key | grep sec | awk '{print $1}' `
 echo "key status is $key_status"
 if [ -z $key_status ]; then
+	read -p "Please follow instructions to create a gpg keypair. Write down the name, email and passphrase you used to create the keypair!! Press enter to start gpg key generation."
 	gpg --generate-key
 else
 	echo "You already have gpg keys!!"
